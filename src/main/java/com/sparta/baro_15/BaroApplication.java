4c5b8bf124ca6c38927eedf6ebc8bb6b9992fbad
syn <tlsdpsk1992@naver.c0m>
2025-07-26 15:51:02 +0900

    [feat] 시작

---
+++ src/main/java/com/sparta/baro_15/BaroApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.baro_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroApplication.class, args);
+    }
+
+}
