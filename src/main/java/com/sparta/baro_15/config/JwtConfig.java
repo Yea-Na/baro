d756cbc047b1cd267696d2c3dcc3ea5c284208eb
syn <tlsdpsk1992@naver.c0m>
2025-07-26 15:53:08 +0900

    [feat] jwt 관련 파일 생성

---
+++ src/main/java/com/sparta/baro_15/config/JwtConfig.java
@@ -0,0 +1,5 @@
+package com.sparta.baro_15.config;
+
+public class JwtConfig {
+
+}
---
+++ src/main/java/com/sparta/baro_15/config/JwtUtil.java
@@ -0,0 +1,11 @@
+package com.sparta.baro_15.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+
+}
