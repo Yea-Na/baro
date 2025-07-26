2b9acfc6fae139c6d7e5428d1e7002abd8669bd1
syn <tlsdpsk1992@naver.c0m>
2025-07-26 16:34:33 +0900

    [feat] passwordEncoder, filterChain 등 기본설정

--- src/main/java/com/sparta/baro_15/config/SecurityConfig.java
+++ src/main/java/com/sparta/baro_15/config/SecurityConfig.java
@@ -3,16 +3,45 @@ package com.sparta.baro_15.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     private final JwtUtil jwtUtil;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                //CSRF 방어 비활성화 (REST API 용)
+                .csrf(csrf -> csrf.disable())
+
+                //세션 관리 (JWT 사용 시 서버가 세션을 생성하거나 사용하지 않음)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                //요청 URL별로 접근 권한을 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/api/auth/**") // 특정 경로를 지정
+                        .permitAll() //인증 없이도 모두 허용
+                        .anyRequest().authenticated() //이외의 요청은 인증된 사용자만 접근 가능
+                )
+                // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 앞에 추가
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
