7ea10132b7bccb969727be601e61c8c8cdd738ad
syn <tlsdpsk1992@naver.c0m>
2025-07-26 19:53:14 +0900

    [chore] 엔티티 작성

--- src/main/java/com/sparta/baro_15/domain/UserEntity.java
+++ src/main/java/com/sparta/baro_15/domain/UserEntity.java
@@ -1,5 +1,38 @@
 package com.sparta.baro_15.domain;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+
+@Entity
+@Table(name = "p_user")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    public UserEntity() {}
+
+    @Builder
+    public UserEntity(String username, String password, String email, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
 }
--- src/main/java/com/sparta/baro_15/domain/UserRole.java
+++ src/main/java/com/sparta/baro_15/domain/UserRole.java
@@ -1,4 +1,12 @@
 package com.sparta.baro_15.domain;
 
 public enum UserRole {
+    USER("사용자"),
+    ADMIN("관리자");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
 }
