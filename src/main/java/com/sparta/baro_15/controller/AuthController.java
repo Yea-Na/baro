fa738fb8967b8f22eee6e5776474699b32d0e8a9
syn <tlsdpsk1992@naver.c0m>
2025-07-27 17:41:13 +0900

    [feat] 엔드포인트 생성

--- src/main/java/com/sparta/baro_15/controller/AuthController.java
+++ src/main/java/com/sparta/baro_15/controller/AuthController.java
@@ -1,26 +1,32 @@
 package com.sparta.baro_15.controller;
 
+import com.sparta.baro_15.dto.reqSignupDto;
 import com.sparta.baro_15.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("")
+@RequestMapping("/v1/api/auth")
 public class AuthController {
 
-    private final AuthService userService;
+    private final AuthService authService;
 
     //회원가입
-    @PostMapping()
-    public void signup(){
-
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody @Valid reqSignupDto reqDto){
+            authService.signUp(reqDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공적으로 완료되었습니다.");
     }
 
     //로그인
-    @PostMapping()
+    @PostMapping("/signin")
     public void signin(){
 
     }
